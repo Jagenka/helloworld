2455258d6935f06a6f7a8edefef640a79052e1da
Johannes Rappelt <johannes@rappelt.de>
2016-11-09 19:41:08 +0100

    test

--- src/HelloGitFuck.java
+++ src/HelloGitFuck.java
@@ -4,5 +4,6 @@ public class HelloGitFuck
 	public static void main(String[] args)
 	{
 		System.out.println("Hello GitFuckingHub!");
+		System.out.println("I'm starting to understand da things!");
 	}
 }
